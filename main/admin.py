a3f2ed96f56892e2db90c211bcdf94454c2d596a
lishchukrostislav <59796149+lishchukrostislav@users.noreply.github.com>
2020-01-16 22:58:47 +0800

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main.models import about_me, link_uch, link_interesting, liberi, link_oge_ege, link_olimpiads, Post_a, Post_g, Caruosel
+from main.models import about_me, link_uch, link_interesting, liberi, link_oge_ege, link_olimpiads, Post_a, Post_g
 
 admin.site.register(about_me)
 admin.site.register(link_uch)
@@ -10,4 +10,4 @@ admin.site.register(link_oge_ege)
 admin.site.register(link_olimpiads)
 admin.site.register(Post_a)
 admin.site.register(Post_g)
-admin.site.register(Caruosel)
+
